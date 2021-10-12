63599376fd6f448ee5de018cd503e628da0d8fd5
ido <kachragiren@gmail.com>
2021-10-12 21:05:07 +0900

    event test

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 const targetCard = document.querySelectorAll(".card");
 
 document.addEventListener("load", open);
+targetCard[0].addEventListener("click", () => { console.log("click") })
 
 function open() {
     targetCard[0].click();
