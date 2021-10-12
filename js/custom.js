c6bcdea859d57a554d4cad4f89ac8a703b39a00c
ido <kachragiren@gmail.com>
2021-10-12 23:48:25 +0900

    xptmxm

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,10 @@
 const targetCard = document.querySelectorAll(".card");
 
 window.addEventListener("load", open);
-targetCard[0].addEventListener("click", () => { console.log("click") })
+targetCard[0].addEventListener("click", () => {
+    document.querySelector("body > div > div > div.wrapper > div:nth-child(1) > div > svg > image").click();
+    console.log("click")
+})
 
 function open() {
     targetCard[0].click();
